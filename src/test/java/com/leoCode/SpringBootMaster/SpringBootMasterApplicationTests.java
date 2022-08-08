a21505a752b89352b13c801e06e8d8eceb91cfbb
leobarrientos02 <leobarrientos02@gmail.com>
2022-08-08 17:01:00 -0400

    Bootstrapping Project

---
+++ src/test/java/com/leoCode/SpringBootMaster/SpringBootMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leoCode.SpringBootMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
